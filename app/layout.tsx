b5099441060872208f8570afe02db258f2a4b4c4
Jan Chalupa <janchalupa@outlook.cz>
2023-03-08 12:34:02 +0100

    chore: WIP.

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 
             <meta
                 name="viewport"
-                content="width=device-width, initial-scale=1.0, user-scalable=0"
+                content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
             />
 
             <html lang="en">
